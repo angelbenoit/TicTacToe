77463a3cf0b772164f8ffbf7bc7a05b94f117b65
Angel Contreras <angelcbenoit@gmail.com>
2018-01-03 13:19:06 -0800

    added ability for user to pick whether they are "X" or "O", also simplified board logic by adding an array and a switch statement

--- game.js
+++ game.js
@@ -1,36 +1,64 @@
 $(document).ready(function () {
 
-    $("#row1col1").click(function () {
-       $(this).fadeIn(1000, function () {
-           $(this).html("X");
-       });
-    });
-    $("#row1col2").on("click", function () {
-        $(this).text("X");
-    });
-    $("#row1col3").on("click", function () {
-        $(this).text("X");
+    var user;
+    var board = [];
+
+
+    //user gets to pick whether they are playing as "X" or "O"
+    $(".btn").click(function () {
+        if($(this).attr("id") === "X"){
+            user = "X";
+            $(this).css("background", "white");
+            $(this).css("color", "black");
+            $("#O").css({"background": "black", "color": "white"});
+        }
+        else{
+            user = "O";
+            $(this).css("background", "white");
+            $(this).css("color", "black");
+            $("#X").css({"background": "black", "color": "white"});
+        }
+
     });
 
 
-    $("#row2col1").on("click", function () {
-        $(this).text("X");
-    });
-    $("#row2col2").on("click", function () {
-        $(this).text("X");
-    });
-    $("#row2col3").on("click", function () {
-        $(this).text("X");
+    //adds user's symbol to the board and the board array
+    $("td").click(function () {
+        var id = $(this).attr("id");
+
+        switch(id){
+            case "row1col1":
+                board[7] = user;
+                break;
+            case "row1col2":
+                board[8] = user;
+                break;
+            case "row1col3":
+                board[9] = user;
+                break;
+            case "row2col1":
+                board[4] = user;
+                break;
+            case "row2col2":
+                board[5] = user;
+                break;
+            case "row2col3":
+                board[6] = user;
+                break;
+            case "row3col1":
+                board[1] = user;
+                break;
+            case "row3col2":
+                board[2] = user;
+                break;
+            case "row3col3":
+                board[3] = user;
+                break;
+        }
+
+        $(this).text(user);
+        console.log(board);
     });
 
 
-    $("#row3col1").on("click", function () {
-        $(this).text("X");
-    });
-    $("#row3col2").on("click", function () {
-        $(this).text("X");
-    });
-    $("#row3col3").on("click", function () {
-        $(this).text("X");
-    });
 });
