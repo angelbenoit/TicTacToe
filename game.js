ef51482baf67074230116eedafbd7f81d229a51e
Angel Contreras <angelcbenoit@gmail.com>
2018-01-04 15:18:37 -0800

    Fixed highlight bug and fixed reset method. The board will reset when the user chooses a new symbol.

--- game.js
+++ game.js
@@ -5,15 +5,24 @@ $(document).ready(function () {
     var boardIDs = ["row1col1", "row1col2", "row1col3",
                     "row2col1", "row2col2", "row2col3",
                     "row3col1", "row3col2", "row3col3"];
+
+    var initialValues = [$("#row1col1").text(), $("#row1col2").text(), $("#row1col3").text(),
+                         $("#row2col1").text(), $("#row2col2").text(), $("#row2col3").text(),
+                         $("#row3col1").text(), $("#row3col2").text(), $("#row3col3").text()];
+
     //user gets to pick whether they are playing as "X" or "O"
     $(".btn").click(function () {
         if($(this).attr("id") === "X"){
+            console.log("YOU PICKED X");
             user = "X";
             pickSymbol(user);
+            resetBoard();
         }
         else{
+            console.log("YOU PICKED O");
             user = "O";
             pickSymbol(user);
+            resetBoard();
         }
     });
 
@@ -27,7 +36,6 @@ $(document).ready(function () {
                 break;
             case "row1col2":
                 check(8, user, id);
-                resetBoard();
                 break;
             case "row1col3":
                 check(9, user, id);
@@ -52,6 +60,8 @@ $(document).ready(function () {
                 break;
         }
 
+        console.log(board);
+
     });
 
 
@@ -67,17 +77,21 @@ $(document).ready(function () {
     function resetBoard() {
         for(var i = 0; i < boardIDs.length; i++){
             board.pop();
-            $(boardIDs[i]).text(board[i]);
+            $("#" + boardIDs[i]).text(initialValues[i]);
         }
     }
 
     function pickSymbol(symbol) {
+        symbol = "#" + symbol;
+        //console.log("changing background color of " + symbol + " to white and text black");
         $(symbol).css("background", "white");
         $(symbol).css("color", "black");
-        if(symbol === "X"){
+        if(symbol === "#X"){
+            //console.log("O is changing");
             $("#O").css({"background": "black", "color": "white"});
         }
         else {
+            //console.log("X is changing");
             $("#X").css({"background": "black", "color": "white"});
         }
     }
