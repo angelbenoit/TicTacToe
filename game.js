2a7355323dec692a739bf175ad8891dbb75ac87b
Angel Contreras <angelcbenoit@gmail.com>
2018-01-05 23:09:42 -0800

    add a method to check whether user has won. If user wins, then there will be a reset button shown. After the user clicks the reset button, the button will be hidden again until another user wins

--- game.js
+++ game.js
@@ -13,23 +13,32 @@ $(document).ready(function () {
     //user gets to pick whether they are playing as "X" or "O"
     $(".btn").click(function () {
         if($(this).attr("id") === "X"){
+            //will hide reset button
+            $("#reset").css("visibility", "hidden");
             console.log("YOU PICKED X");
             user = "X";
             pickSymbol(user);
             resetBoard();
         }
-        else{
+        else if($(this).attr("id") === "O"){
+            $("#reset").css("visibility", "hidden");
             console.log("YOU PICKED O");
             user = "O";
             pickSymbol(user);
+            resetBoard();
+        }
+        else{
+            $("#reset").css("visibility", "hidden");
             resetBoard();
         }
     });
 
     //adds user's symbol to the board and the board array
     $("td").click(function () {
+        //stores the id (where in the grid the user clicked) in a variable
         var id = $(this).attr("id");
 
+        //stores that id , user and array index into the check method
         switch(id){
             case "row1col1":
                 check(7, user, id);
@@ -61,13 +70,14 @@ $(document).ready(function () {
         }
 
         console.log(board);
-
+        checkForWin(user);
     });
 
 
+    //this method will
     function check(spot, value, Id){
-        if(board[spot] !== "X" && board[spot] !== "O"){
-            board[spot] = value;
+        if(board[spot-1] !== "X" && board[spot-1] !== "O"){
+            board[spot-1] = value;
             var val = "#" + Id;
             //console.log(val + " " + value);
             $(val).text(value);
@@ -88,12 +98,56 @@ $(document).ready(function () {
         $(symbol).css("color", "black");
         if(symbol === "#X"){
             //console.log("O is changing");
-            $("#O").css({"background": "black", "color": "white"});
+            $("#O").css({"background": "black", "color": "palegreen"});
         }
         else {
             //console.log("X is changing");
-            $("#X").css({"background": "black", "color": "white"});
+            $("#X").css({"background": "black", "color": "palegreen"});
         }
     }
+
+    function checkForWin(item) {
+        console.log("Checking...");
+        var win = false;
+        if(board[0] === item && board[1] === item && board[2] === item){
+            console.log(user + " IS THE WINNER: ROW 3");
+            win = true;
+        }
+        else if(board[3] === item && board[4] === item && board[5] === item){
+            console.log(user + " IS THE WINNER: ROW 2");
+            win = true;
+        }
+        else if(board[6] === item && board[7] === item && board[8] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+        else if(board[0] === item && board[3] === item && board[6] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+        else if(board[1] === item && board[4] === item && board[7] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+        else if(board[2] === item && board[5] === item && board[8] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+        else if(board[0] === item && board[4] === item && board[8] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+        else if(board[2] === item && board[4] === item && board[6] === item){
+            console.log(user + " IS THE WINNER: ROW 1");
+            win = true;
+        }
+
+        if(win){
+            $("#reset").css("visibility", "visible");
+        }
+
+    }
+
+
 
 });
